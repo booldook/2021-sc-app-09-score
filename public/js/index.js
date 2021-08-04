e1378407386ebea5ab8a017ccf83dfe36e4f82eb
booldook <booldook@gmail.com>
2021-08-04 14:48:46 +0900

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -3,22 +3,34 @@ console.log(firebase);
 var auth = firebase.auth();
 var db = firebase.database();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
-
-console.log(auth, db, googleAuth);
+// console.log(auth, db, googleAuth);
 
 
 /************** function init ***************/
 
 
 /************** event callback ***************/
-function onAuthChanged(user) {
+function onAuthChanged(user) { // auth상태가 변하면 알려줘
 	console.log(user);
+	if(user) {
+		$('.bt-login').hide();
+		$('.bt-logout').show();
+	}
+	else {
+		$('.bt-login').show();
+		$('.bt-logout').hide();
+	}
 }
 
 function onLogin() {
 	auth.signInWithPopup(googleAuth);
 }
 
+function onLogout() {
+	auth.signOut();
+}
+
 /************** event init ***************/
-auth.onAuthStateChanged(onAuthChanged);
+auth.onAuthStateChanged(onAuthChanged); // auth상태가 변하면 알려줘
 $('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
