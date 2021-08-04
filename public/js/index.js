92f289efad9c38c05c77ce85d1b903baf2449d88
booldook <booldook@gmail.com>
2021-08-04 15:04:32 +0900

    Google Login 완료

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+/* 
+false 판정: 0, null, undefined, '', false
+true 판정: false판정 외 모든것
+- 0을 제외한 모든 숫자
+- ''(빈문자열)을 제외한 모든 문자
+- 객체, 함수 ... 전부다
+*/
+
 /************** global init ***************/
 console.log(firebase);
 var auth = firebase.auth();
@@ -15,10 +23,15 @@ function onAuthChanged(user) { // auth상태가 변하면 알려줘
 	if(user) {
 		$('.bt-login').hide();
 		$('.bt-logout').show();
+		$('.photo-logo img').attr('src', user.photoURL);
+		$('.photo-logo').show();
+		$('.icon-logo').hide();
 	}
 	else {
 		$('.bt-login').show();
 		$('.bt-logout').hide();
+		$('.photo-logo').hide();
+		$('.icon-logo').show();
 	}
 }
 
