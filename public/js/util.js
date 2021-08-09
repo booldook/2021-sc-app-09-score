e7b327f89186edd8cf3c15b64e6c31d0a46d8a3f
booldook <booldook@gmail.com>
2021-08-09 10:23:21 +0900

    03.game 디자인 작업 중

--- public/js/util.js
+++ public/js/util.js
@@ -4,11 +4,10 @@ function random(startNumber, count) {
 }
 
 // 0 ~ 9를 받으면 '0'을 더해주는 함수
+// const zp2 = n => n < 10 ? '0'+n : n; // ES6 함수
 function zp(n) {
 	// 삼항 연산자 var a = 조건 ? 실행(참) : 실행(거짓)
 	// if(n < 10) return '0'+n;
 	// else return n;
 	return (n < 10) ? '0'+n : n;
 }
-
-// const zp2 = n => n < 10 ? '0'+n : n; // ES6 함수
